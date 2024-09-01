7189cf767768606e882ed4e5f19efd6863b7d1ac
wjorden <jorden.williams.1989@gmail.com>
2024-09-01 14:42:49 -0400

    setup components, initial layout, successful test launch

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
     "fmt"
+    "net/http"
+
+    "github.com/a-h/templ"
+    "github.com/CaffeineOrDeath/banner-gen/internal/views"
 )
 
 func main(){
-    fmt.Println("Hello, World!")
+    component := views.Layout("Home")
+    http.Handle("/", templ.Handler(component))
+
+    fmt.Println("Listening on :42069")
+    http.ListenAndServe(":42069", nil) //iykyk
 }
